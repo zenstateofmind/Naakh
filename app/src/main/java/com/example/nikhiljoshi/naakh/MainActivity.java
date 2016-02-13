2c733bf7e994327de6e6072ceb57887fe92a3f6e
zenstateofmind <nikhil.joshi92@gmail.com>
2016-02-13 15:00:19 -0800

    Welcome page with sign in and register button

--- app/src/main/java/com/example/nikhiljoshi/naakh/MainActivity.java
+++ app/src/main/java/com/example/nikhiljoshi/naakh/MainActivity.java
@@ -2,11 +2,7 @@ package com.example.nikhiljoshi.naakh;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -27,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
 //                        .setAction("Action", null).show();
 //            }
 //        });
-        Intent intent = new Intent(this, SignIn.class);
+        Intent intent = new Intent(this, Welcome.class);
         startActivity(intent);
     }
 
---
+++ app/src/main/java/com/example/nikhiljoshi/naakh/Welcome.java
@@ -0,0 +1,35 @@
+package com.example.nikhiljoshi.naakh;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
