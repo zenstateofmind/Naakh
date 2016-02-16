3648c999abb61407a9478c9eb58ba84342fcce4e
zenstateofmind <nikhil.joshi92@gmail.com>
2016-02-16 07:28:23 -0800

    Setup volley, get login and sign in with it

--- app/src/main/java/com/example/nikhiljoshi/naakh/SignIn.java
+++ app/src/main/java/com/example/nikhiljoshi/naakh/SignIn.java
@@ -1,12 +1,29 @@
 package com.example.nikhiljoshi.naakh;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignIn extends AppCompatActivity {
 
@@ -21,14 +38,42 @@ public class SignIn extends AppCompatActivity {
     }
 
     public void signInValidation(View view) {
-        final EditText usernameView = (EditText) findViewById(R.id.username);
-        final EditText passwordView = (EditText) findViewById(R.id.password);
-        final String username = usernameView.getText().toString();
-        final String password = passwordView.getText().toString();
-        //user volley to send to naakh server and see if i can get a client id
-        // Once I get a client id save it in shared preferences
+        final String username = getStringFromEditText(R.id.username);
+        final String password = getStringFromEditText(R.id.password);
+        final VolleyInstance volley = VolleyInstance.getInstance(this.getApplicationContext());
+        final String loginUrl = "https://naakh.herokuapp.com/api/v1/oauth2/access_token";
+        final String oauth_client_secret = "1f022ef12e35f86c2f02f1b9988b899a9cd7de02";
+        final String oauth_client_id = "b73fa9950d135f4cdf21";
 
+        StringRequest request = new StringRequest(Request.Method.POST, loginUrl, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+//                loginSuccess(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "failure", Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("phone_number", "123456789");
+                params.put("password", "password");
+                params.put("oauth_client_id", oauth_client_id);
+                params.put("oauth_client_secret", oauth_client_secret);
+                return params;
+            }
+        };
+        // Once I get a client id save it in shared preferences
+        volley.getRequestQueue().add(request);
         // Then open translations class
 
+    }
+
+    private String getStringFromEditText(int id) {
+        EditText view = (EditText) findViewById(id);
+        return view.getText().toString();
     }
 }
