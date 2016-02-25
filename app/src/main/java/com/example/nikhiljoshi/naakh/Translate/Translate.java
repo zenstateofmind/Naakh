04c7670ecb67da29a25338efbab8ddfffb6564c3
zenstateofmind <nikhil.joshi92@gmail.com>
2016-02-24 20:34:39 -0800

    Clean up the ui and other stuff
    other stuff:
    1) Back button translate and welcome page goes back to homepage
    2) sign in with phone number — make it a better experience

--- app/src/main/java/com/example/nikhiljoshi/naakh/Translate/Translate.java
+++ app/src/main/java/com/example/nikhiljoshi/naakh/Translate/Translate.java
@@ -43,6 +43,14 @@ public class Translate extends AppCompatActivity {
     }
 
     @Override
+    public void onBackPressed() {
+        Intent startMain = new Intent(Intent.ACTION_MAIN);
+        startMain.addCategory(Intent.CATEGORY_HOME);
+        startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(startMain);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
         int id = item.getItemId();
--- app/src/main/java/com/example/nikhiljoshi/naakh/Welcome/Welcome.java
+++ app/src/main/java/com/example/nikhiljoshi/naakh/Welcome/Welcome.java
@@ -41,4 +41,12 @@ public class Welcome extends AppCompatActivity {
         Intent intent = new Intent(this, SignIn.class);
         startActivity(intent);
     }
+
+    @Override
+    public void onBackPressed() {
+        Intent startMain = new Intent(Intent.ACTION_MAIN);
+        startMain.addCategory(Intent.CATEGORY_HOME);
+        startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(startMain);
+    }
 }
