a0223b6919a2a2f3dc6601891b6d12881ea85c89
zenstateofmind <nikhil.joshi92@gmail.com>
2016-02-28 15:48:27 -0800

    Profile page with fragments

---
+++ app/src/main/java/com/example/nikhiljoshi/naakh/Profile/ProfileFragment.java
@@ -0,0 +1,10 @@
+package com.example.nikhiljoshi.naakh.Profile;
+
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by nikhiljoshi on 2/28/16.
+ */
+public class ProfileFragment extends Fragment {
+}
