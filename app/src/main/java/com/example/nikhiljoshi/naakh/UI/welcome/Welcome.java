5fff434015d9f84098af80cdac6a8d29732bab4b
zenstateofmind <nikhil.joshi92@gmail.com>
2016-03-06 18:11:15 -0500

    Random changes

--- app/src/main/java/com/example/nikhiljoshi/naakh/UI/welcome/Welcome.java
+++ app/src/main/java/com/example/nikhiljoshi/naakh/UI/welcome/Welcome.java
@@ -22,6 +22,7 @@ public class Welcome extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
+
     }
 
     public void openActivity(View view) {
